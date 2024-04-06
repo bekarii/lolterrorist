e93961e66899b7c80ebaf55afd2391c5b9129c08
bekarii <80601224+bekarii@users.noreply.github.com>
2024-04-07 01:57:11 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleBackgroundButton = document.getElementById("toggleBackground");
+    const backgroundElement = document.querySelector(".background");
+
+    toggleBackgroundButton.addEventListener("click", function() {
+        // Sprawdzamy, czy tło to film czy obrazek
+        if (backgroundElement.classList.contains("image")) {
+            // Jeśli tło to obrazek, zmieniamy na film
+            backgroundElement.style.backgroundImage = "url('theme.mp4')";
+            backgroundElement.classList.remove("image");
+        } else {
+            // Jeśli tło to film, zmieniamy na obrazek
+            backgroundElement.style.backgroundImage = "url('background.jpg')";
+            backgroundElement.classList.add("image");
+        }
+    });
+});
