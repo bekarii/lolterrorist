76a351dd616ae722be267779d3a9398827ea2fb5
bekarii <80601224+bekarii@users.noreply.github.com>
2024-04-07 03:41:56 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const backgroundMusic = document.getElementById('backgroundMusic');
-  const toggleSoundButton = document.getElementById('toggleSound');
-  const soundIcon = document.getElementById('soundIcon');
-
-  let isSoundOn = true;
-
-  toggleSoundButton.addEventListener('click', function () {
-    isSoundOn = !isSoundOn;
-    if (isSoundOn) {
-      backgroundMusic.play();
-    } else {
-      backgroundMusic.pause();
-    }
-    soundIcon.src = isSoundOn ? 'sound_on.png' : 'sound_off.png';
-  });
-
-  // Set default volume
-  backgroundMusic.volume = 0.4;
-
   const toggleBackgroundButton = document.getElementById('toggleBackground');
   const container = document.querySelector('.container');
 
