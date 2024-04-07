f32772676ba6768834337e1d961d942b36752f12
bekarii <80601224+bekarii@users.noreply.github.com>
2024-04-07 03:12:07 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const toggleBackgroundButton = document.getElementById("toggleBackground");
-    const backgroundElement = document.querySelector(".background");
+const background = document.querySelector('.background');
+const toggleBackgroundButton = document.getElementById('toggleBackground');
+const toggleSoundButton = document.getElementById('toggleSound');
+const soundIcon = document.getElementById('soundIcon');
+let isBackgroundVideo = true;
+let isSoundOn = true;
 
-    // Ustawienie tła strony na obrazek domyślnie
-    backgroundElement.style.backgroundImage = "url('background.jpg')";
-    backgroundElement.classList.add("image");
+// Toggle background video
+toggleBackgroundButton.addEventListener('click', () => {
+  isBackgroundVideo = !isBackgroundVideo;
+  background.style.backgroundImage = isBackgroundVideo ? "url('theme.mp4')" : "url('background.jpg')";
+});
 
-    toggleBackgroundButton.addEventListener("click", function() {
-        // Sprawdzamy, czy tło to film czy obrazek
-        if (backgroundElement.classList.contains("image")) {
-            // Jeśli tło to obrazek, zmieniamy na film
-            backgroundElement.style.backgroundImage = "url('theme.mp4')";
-            backgroundElement.classList.remove("image");
-        } else {
-            // Jeśli tło to film, zmieniamy na obrazek
-            backgroundElement.style.backgroundImage = "url('background.jpg')";
-            backgroundElement.classList.add("image");
-        }
-    });
+// Toggle sound
+toggleSoundButton.addEventListener('click', () => {
+  isSoundOn = !isSoundOn;
+  soundIcon.src = isSoundOn ? 'sound_on.png' : 'sound_off.png';
+  // Adjust volume accordingly
+  // Assuming you have an audio element with id 'backgroundMusic'
+  const backgroundMusic = document.getElementById('backgroundMusic');
+  backgroundMusic.volume = isSoundOn ? 0.4 : 0;
 });
